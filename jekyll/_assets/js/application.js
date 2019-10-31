ec177958fdc04cd3193330adba0ce7489b58300c
Yohsuke Fukai <ysk@yfukai.net>
2019-10-31 20:53:32 +0900

    deploy to github

--- jekyll/_assets/js/application.js
+++
@@ -1,5 +0,0 @@
-//= require jquery-3.2.1.min
-//= require popper-1.12.7
-//= require bootstrap
-//= require extlink
-
--- jekyll/_assets/js/extlink.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $("a").not(".samewindow").attr("target","_new");
-});
-
